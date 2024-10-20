74346115954ddb1b4a62b6b6d9f17d31ef749949
TesseractPi <149723729+TesseractPi@users.noreply.github.com>
2024-10-20 14:31:05 -0700

    Add actual functionality

--- removinator.js
+++ removinator.js
@@ -1,2 +1,11 @@
-console.log("it works!!1")
-// I'll put more things here once I find out how Google Ai displays itself
+function remove(){
+	try{
+		document.getElementById('Odp5De').innerHTML = '';
+	}
+	catch(err){
+		// skip if ai isnt on the page
+	}
+	console.log('Bye bye AI! ');
+	setTimeout(function(){remove();},10000);
+}
+remove();
